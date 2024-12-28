c70a5c27be1f8808cf069190e734e2a760571010
kuanghua <869653722@qq.com>
2024-12-28 17:04:05 +0800

    fix: some issue of fix

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,49 +1,53 @@
 <template>
-   <div>index.vue</div>
+  <div class="scroll-y pt-20px pl-20px">
+    <h2 class="mb-10px">一个能为企业真正提效的低代码平台</h2>
+    <div>具备的特点：开发周期短，简单，高效，代码无嵌入性</div>
+
+    <div class="mt-40px mb-10px">
+      <a class="btn-click-style font-bold" href="https://gitee.com/jzfai/low-generator-doc.git">详细介绍文档</a>
+    </div>
+
+    <h2 class="mb-10px mt-40px">目前提供了两个平台的模板</h2>
+    <div>vue3-admin-plus(vue3-admin系列的都能使用)</div>
+    <div>micro-admin-single（单表和多表增删改查生成模板）</div>
+
+    <div class="mt-20px">
+      如何你感兴趣也可以一参与参与
+      <a
+          class="btn-click-style font-bold"
+          href="https://github.jzfai.top/low-code-platform/#/template-file/template-file-list"
+      >
+        模板开发
+      </a>
+    </div>
+
+  </div>
 </template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+import  Test from "./Test.vue"
+import { useConfigStore } from '@/store/config'
+import { dillSwagger3ByParams, dillSwagger3ToUse } from '@/hooks/use-swagger-analysis'
+
+const { theme, size } = useConfigStore()
+const route = useRoute()
 
 
-<script setup>
-//import {onMounted, getCurrentInstance, watch,ref,toRefs,reactive,computed} from "vue";
-//获取store和router
-// import {useRouter} from 'vue-router'
-// import {useStore} from 'vuex'
-//let {proxy} = getCurrentInstance();
-// const props = defineProps({
-//   name: {
-//     require: true,
-//     default: "fai",
-//     type:String,
-//   },
-// });
-// const state = reactive({
-//   levelList: null
-// });
+const arr = [{name: "123", age : 123}, {name:"1234432", age : 1234324}, {name:"1234432", age : 1234324}]
 
-//const routes = computed(() => {
-//    return proxy.$store.state.permission.routes;
-//  });
-// watch(() => props.name, (oldValue,newValue) => {
-//
-//   },
-//   { immediate: true }
-// );
+const filterObj={}
+arr.forEach(f=>{
+  if(!filterObj[f.name]){
+    filterObj[f.name]=f.name
+  }else{
+    console.log(`${filterObj[f.name]}重复了`)
+  }
+})
 
-// const store = useStore()
-// const router = useRouter()
-// onMounted(()=>{
-//   console.log("页面挂载了")
-// })
-// let helloFunc = () => {
-//   console.log("helloFunc");
-// };
-//导出给refs使用
-// defineExpose({ helloFunc });
-//导出属性到页面中使用
-// let {levelList} = toRefs(state);
+const testFunc=()=>{
+  console.log("我是父元素函数")
+}
+
+defineExpose({ arr,testFunc });
 </script>
-
-
-<style scoped lang="scss">
-
-</style>
